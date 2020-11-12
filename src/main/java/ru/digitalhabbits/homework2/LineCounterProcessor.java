2d04d2689e6e968e4aa8059c5581711e7a279875
Oksana Churkina <ochurkina@parseq.pro>
2020-11-13 00:04:01 +0300

    Homework2 implementation

--- src/main/java/ru/digitalhabbits/homework2/LineCounterProcessor.java
+++ src/main/java/ru/digitalhabbits/homework2/LineCounterProcessor.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 
 import javax.annotation.Nonnull;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static java.lang.System.currentTimeMillis;
 import static org.slf4j.LoggerFactory.getLogger;
@@ -16,19 +17,17 @@ public class LineCounterProcessor
     private static final int BASE_DELAY = 200;
     private static final int RANDOM_DELAY = 200;
 
-    private final Random random = new Random(currentTimeMillis());
-
     @Nonnull
     @Override
     public Pair<String, Integer> process(@Nonnull String line) {
         randomSleep();
-        // TODO: NotImplemented: подсчет кол-ва символов в строке + произвольная задержка randomSleep()
-        return null;
+        int count = line.toCharArray().length;
+        return Pair.of(line, count);
     }
 
     private void randomSleep() {
         try {
-            Thread.sleep(BASE_DELAY + random.nextInt(RANDOM_DELAY));
+            Thread.sleep(BASE_DELAY + ThreadLocalRandom.current().nextInt(RANDOM_DELAY));
         } catch (InterruptedException exception) {
             logger.warn("", exception);
         }
